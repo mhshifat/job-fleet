5b9675793c9b2d7e5ff24e206376de03335accbb
shifat.dev <adnan@lazertechnologies.com>
2024-10-07 23:46:11 +0600

    added auth guard for apis

--- src/app/api/jobs/[id]/route.ts
+++ src/app/api/jobs/[id]/route.ts
@@ -2,8 +2,11 @@ import { APIResponse } from "@/utils/types";
 import { NextResponse } from "next/server";
 import { getJobById } from "../../../../../actions/job";
 import { asyncErrorHandler } from "@/utils/error";
+import { isAuthenticated } from "../../../../../actions/auth";
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
+  const payload = await isAuthenticated();
+  if (!payload?.data?.uid) throw new Error("401:-Unauthorized");
   return asyncErrorHandler(async () => {
     const jobId = params.id;
     const job = await getJobById(jobId);
--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -2,8 +2,12 @@ import { APIResponse } from "@/utils/types";
 import { NextResponse } from "next/server";
 import { createJob } from "../../../../actions/job";
 import { asyncErrorHandler } from "@/utils/error";
+import { isAuthenticated } from "../../../../actions/auth";
 
 export async function POST(req: Request) {
+  const payload = await isAuthenticated();
+  if (!payload?.data?.uid) throw new Error("401:-Unauthorized");
+
   return asyncErrorHandler(async () => {
     const json = await req.json();
     const job = await createJob(json);
