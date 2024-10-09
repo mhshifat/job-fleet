d7ee098ea6a8e2bb2b77c8420b3bd360736a0bb6
shifat.dev <adnan@lazertechnologies.com>
2024-10-09 22:23:11 +0600

    fixed logout issues

--- src/domain/auth/use-sign-out-mutation.ts
+++ src/domain/auth/use-sign-out-mutation.ts
@@ -2,20 +2,27 @@ import { toast } from "@/utils/toast";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { authService } from "@/infra/auth/service";
 import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { ROUTE_PATHS } from "@/utils/constants";
+import { useAuth } from "@/components/providers/auth";
 
 type ResponseType = string;
 
 export default function useSignOutMutation() {
+  const router = useRouter();
   const queryClient = useQueryClient();
+  const { updateAuthState } = useAuth();
   
   return useMutation<ResponseType, Error>({
     mutationFn: async () => {
+      queryClient.clear();
       const data = await authService.logout();
       return data || "";
     },
-    onSuccess: (data) => {
+    onSuccess: async (data) => {
+      updateAuthState(null);
       toast.success(data || "Successfully signed out!");
-      queryClient.invalidateQueries({ queryKey: ["me"] });
+      router.push(ROUTE_PATHS.HOME);
     },
     onError: (err) => {
       if (err instanceof AxiosError) {
